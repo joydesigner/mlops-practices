23d47daed8fbf344ceb8319c3fba11b5161045d0
Jason Z <jason.zhengxin@gmail.com>
2024-09-09 20:29:12 +1000

    fixing the unit test

--- training/train.py
+++ training/train.py
@@ -9,45 +9,37 @@ import lightgbm
 
 def split_data(data_df):
     """Split a dataframe into training and validation datasets"""
-
     features = data_df.drop(['target', 'id'], axis=1)
     labels = np.array(data_df['target'])
     features_train, features_valid, labels_train, labels_valid = \
-        train_test_split(features, labels, test_size=0.2,
-                         random_state=0)
+        train_test_split(features, labels, test_size=0.2, random_state=0)
 
     train_data = lightgbm.Dataset(features_train, label=labels_train)
-    valid_data = lightgbm.Dataset(
-        features_valid,
-        label=labels_valid,
-        free_raw_data=False)
+    valid_data = lightgbm.Dataset(features_valid, label=labels_valid, free_raw_data=False)
 
-    return (train_data, valid_data)
+    return (train_data, valid_data, features_train, features_valid)
 
 
 # Train the model, return the model
 def train_model(data, parameters):
     """Train a model with the given datasets and parameters"""
-    train_data, valid_data = data
-
+    train_data, valid_data, _, _ = data
     model = lightgbm.train(parameters,
                            train_data,
                            valid_sets=[valid_data],
                            num_boost_round=500,
                            early_stopping_rounds=20)
-
     return model
 
 
 # Evaluate the metrics for the model
 def get_model_metrics(model, data):
     """Construct a dictionary of metrics for the model"""
-    predictions = model.predict(data[1].data)
-    fpr, tpr, thresholds = metrics.roc_curve(data[1].label, predictions)
+    _, valid_data, _, _ = data
+    predictions = model.predict(valid_data.data)
+    fpr, tpr, thresholds = metrics.roc_curve(valid_data.label, predictions)
     model_metrics = {
-        "auc": (
-            metrics.auc(
-                fpr, tpr))}
+        "auc": metrics.auc(fpr, tpr)
+    }
     print(model_metrics)
-
     return model_metrics
--- training/train_test.py
+++ training/train_test.py
@@ -19,16 +19,23 @@ def sample_data():
     }
     return pd.DataFrame(data=test_data)
 
-def test_split_data(sample_data):
-    train_data, valid_data = split_data(sample_data)
+def test_split_data():
+    test_data = {
+        'id': [0, 1, 2, 3, 4],
+        'target': [0, 0, 1, 0, 1],
+        'col1': [1, 2, 3, 4, 5],
+        'col2': [2, 1, 1, 2, 1]
+    }
+    data_df = pd.DataFrame(data=test_data)
+    train_data, valid_data, features_train, features_valid = split_data(data_df)
 
     # verify that columns were removed correctly
-    assert set(train_data.data.columns) == {'col1', 'col2'}
-    assert set(valid_data.data.columns) == {'col1', 'col2'}
+    assert set(features_train.columns) == {'col1', 'col2'}
+    assert set(features_valid.columns) == {'col1', 'col2'}
 
     # verify that data was split as desired (80% train, 20% valid)
-    assert train_data.num_data() == 4
-    assert valid_data.num_data() == 1
+    assert features_train.shape[0] == 4
+    assert features_valid.shape[0] == 1
 
 def test_train_model():
     data = __get_test_datasets()
@@ -57,21 +64,30 @@ def test_train_model():
     # verify that the model has been trained
     assert model.num_trees() > 0
 
+def test_train_model():
+    data = __get_test_datasets()
+    params = {
+        "learning_rate": 0.05,
+        "metric": "auc",
+        "min_data": 1
+    }
+    model = train_model(data, params)
+    # verify that parameters are passed in to the model correctly
+    for param_name in params.keys():
+        assert param_name in model.params
+        assert params[param_name] == model.params[param_name]
+
 def test_get_model_metrics():
     class MockModel:
         @staticmethod
         def predict(data):
-            return np.array([0.1, 0.9])
-
+            return np.array([0, 0])
     data = __get_test_datasets()
-
     metrics = get_model_metrics(MockModel(), data)
-
     # verify that metrics is a dictionary containing the auc value.
     assert "auc" in metrics
     auc = metrics["auc"]
-    expected_auc = roc_auc_score(data[1].label, [0.1, 0.9])
-    np.testing.assert_almost_equal(auc, expected_auc, decimal=5)
+    np.testing.assert_almost_equal(auc, 0.5)
 
 def __get_test_datasets():
     """This is a helper function to set up some test data"""
@@ -79,7 +95,6 @@ def __get_test_datasets():
     y_train = np.array([1, 1, 0, 1, 0, 1])
     X_test = np.array([7, 8]).reshape(-1, 1)
     y_test = np.array([0, 1])
-
     train_data = lightgbm.Dataset(X_train, y_train)
     valid_data = lightgbm.Dataset(X_test, y_test)
-    return (train_data, valid_data)
+    return (train_data, valid_data, pd.DataFrame(X_train), pd.DataFrame(X_test))
